32200f9fbab8f4ca76cebc68c323d8d9ab08a938
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-10-25 19:52:59 -0300

    Update InstallHass.io.sh

--- InstallHass.io.sh
+++ InstallHass.io.sh
@@ -55,48 +55,55 @@ echo ""
 
 
 echo "[Info] Create folders for mosquitto"
-sudo mkdir /mosquitto
-sudo mkdir /mosquitto/config
-sudo mkdir /mosquitto/data
-sudo mkdir /mosquitto/log
-sudo chmod 757 mosquitto
-sudo chmod 757 mosquitto/config
-#sudo chmod 755 mosquitto/data
-#sudo chmod 755 mosquitto/log
+sudo mkdir /usr/share/hassio/share/mosquitto/
+sudo mkdir /usr/share/hassio/share/mosquitto/config/
+sudo mkdir /usr/share/hassio/share/mosquitto/data
+sudo mkdir /usr/share/hassio/share/mosquitto/config/log
 
-echo "[Info] Download mosquitto.conf for "
-sudo curl -sL https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/mosquitto.conf > "/mosquitto/config/mosquitto.conf"
-                                                                                                # "/var/lib/docker/volumes/mosquitto.conf" #quando eu não passa o parematro -v 
+sudo chmod 755 /usr/share/hassio/share/mosquitto/
+sudo chmod 755 /usr/share/hassio/share/mosquitto/config/
+sudo chmod 755 /usr/share/hassio/share/mosquitto/data
+sudo chmod 755 /usr/share/hassio/share/mosquitto/config/log
 
-echo "[Info] Run mosquitto on docker "
-sudo docker run -it --name=mosquitto -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v /mosquitto/:/mosquitto/ eclipse-mosquitto
-# sudo docker run -it --name=mosquitto -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
-# Example: sudo docker run -it –name=“mosquitto” –restart on-failure -p 1883:1883 -p 9001:9001 -p 1883:8884 -p 1883:8883 -p 9001:9001 -v /srv/mqtt/config:/mqtt/config:ro  -v /srv/mqtt/log:/mqtt/log  -v /srv/mqtt/data/:/mqtt/data/ eclipse-mosquitto
+echo "[Info] Download addMosquitto.conf for "
+sudo curl -sL https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/mosquitto.conf > "/usr/share/hassio/share/mosquitto/config/addMosquitto.conf"
+sudo chmod 755 /usr/share/hassio/share/mosquitto/config/addMosquitto.conf
 
+#echo ""
+#echo ""
+#echo "[Info] Run mosquitto on docker "
+           sudo docker run -it --name=mosquitto -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v /mosquitto/:/mosquitto/ -v /mosquitto/log:/mosquitto/log -v /mosquitto/data/:/mosquitto/data/  eclipse-mosquitto
+# Example: sudo docker run -it --name=“mosquitto” –restart on-failure -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v /srv/mqtt/config:/mqtt/config:ro  -v /srv/mqtt/log:/mqtt/log  -v /srv/mqtt/data/:/mqtt/data/ eclipse-mosquitto
+# Example: sudo docker run -it --name=mosquitto                       -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
 
+echo ""
+echo ""
 echo "Baixar o Portainerio" 
-sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /path/on/host/data:/data portainer/portainer
-#docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /path/to/certs:/certs -v portainer_data:/data portainer/portainer -H tcp://<DOCKER_HOST>:<DOCKER_PORT> --tlsverify
+ sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /path/on/host/data:/data portainer/portainer
+#sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /path/to/certs:/certs -v portainer_data:/data portainer/portainer -H tcp://<DOCKER_HOST>:<DOCKER_PORT> --tlsverify
 echo ""
 echo ""
 
-echo "[Info] Referencia de erros:"
-echo "Para imagem docker https://hub.docker.com/_/eclipse-mosquitto"
-echo "Para problemas de pasta com o parametro -v... https://github.com/eclipse/mosquitto/issues/457"
-echo ""
-echo ""
-echo "[Info] Para Configuracoes no hassio"
-echo "https://www.home-assistant.io/docs/mqtt/broker#run-your-own"
-echo "https://community.home-assistant.io/t/solved-connect-ha-to-mqtt-broker-with-tls-ca-crt/158415"
-echo ""
-echo ""
-echo "[Info] Para certificados"
-echo "http://www.steves-internet-guide.com/mosquitto-tls/"
-echo "https://www.youtube.com/watch?v=f3f4h7q6x5g"
-echo "https://www.youtube.com/watch?v=zPT8LFWqazM"
-echo ""
-echo ""
-echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/"
-echo "(replace yourIPaddress with your actual IP address) from your web browser to access your new Home Assistant installation."
+
+#echo ""
+#echo ""
+#echo "[Info] Referencia de erros:"
+#echo "Para imagem docker https://hub.docker.com/_/eclipse-mosquitto"
+#echo "Para problemas de pasta com o parametro -v... https://github.com/eclipse/mosquitto/issues/457"
+#echo ""
+#echo ""
+#echo "[Info] Para Configuracoes no hassio"
+#echo "https://www.home-assistant.io/docs/mqtt/broker#run-your-own"
+#echo "https://community.home-assistant.io/t/solved-connect-ha-to-mqtt-broker-with-tls-ca-crt/158415"
+#echo ""
+#echo ""
+#echo "[Info] Para certificados"
+#echo "http://www.steves-internet-guide.com/mosquitto-tls/"
+#echo "https://www.youtube.com/watch?v=f3f4h7q6x5g"
+#echo "https://www.youtube.com/watch?v=zPT8LFWqazM"
+#echo ""
+#echo ""
+#echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/"
+#echo "(replace yourIPaddress with your actual IP address) from your web browser to access your new Home Assistant installation."
 
 
