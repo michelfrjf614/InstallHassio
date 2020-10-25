2108ed20f7a92c20b303ed8c04b0a170d9c809b7
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-10-25 13:26:36 -0300

    Update InstallHass.io.sh

--- InstallHass.io.sh
+++ InstallHass.io.sh
@@ -49,99 +49,33 @@ echo ""
 echo ""
 echo "[Info] Install Home Assistant from latest script from Home assistant GitHub (working as of Sept. 2020)"
 sudo curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | bash -s
-
 echo ""
 echo ""
 echo ""
 echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/"
 echo "(replace yourIPaddress with your actual IP address) from your web browser to access your new Home Assistant installation."
-
-
-echo "[Info] Create folders for mosquitto"
-echo "sudo mkdir /mosquitto"
-echo "sudo mkdir /mosquitto/config"
-echo "sudo mkdir /mosquitto/data"
-echo "sudo mkdir /mosquitto/log"
-echo "sudo chmod 755 mosquitto"
-echo "sudo chmod 755 mosquitto/config"
-echo "sudo chmod 755 mosquitto/data"
-echo "sudo chmod 755 mosquitto/log"
-
-echo "[Info] Download mosquitto.conf for "
-sudo curl -sL https://raw.githubusercontent.com/eclipse/mosquitto/master/mosquitto.conf > "/mosquitto/config/mosquitto.conf"
-
-
-
-echo "sudo docker run -it -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto"
 echo ""
 echo ""
-echo "Configuration add the following to mosquitto.conf in"
-echo ""
-echo "nano /usr/share/mosquitto/config/mosquitto.conf"
-echo ""
-echo "persistence true"
-echo "persistence_location /mosquitto/data/"
-echo "log_dest file /mosquitto/log/mosquitto.log"
-
-echo "# Add TLS protected MQTT"
-echo "listener 8883"
-echo "cafile  /ssl/chain.pem"
-echo "certfile  /ssl/fullchain.pem"
-echo "keyfile  /ssl/privkey.pem"
-echo ""
-echo "listener 9001"
-echo "protocol websockets"
-echo ""
-echo "# Add TLS protected MQTT via websockets"
-echo "listener 8443"
-echo "protocol websockets"
-echo "cafile  /ssl/chain.pem"
-echo "certfile  /ssl/fullchain.pem"
-echo "keyfile  /ssl/privkey.pem"
-
-
 echo "Baixar o Portainerio" 
 echo "docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /path/on/host/data:/data portainer/portainer"
-echo "docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always  -v /path/to/certs:/certs -v portainer_data:/data portainer/portainer -H tcp://<DOCKER_HOST>:<DOCKER_PORT> --tlsverify"
+echo "docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /path/to/certs:/certs -v portainer_data:/data portainer/portainer -H tcp://<DOCKER_HOST>:<DOCKER_PORT> --tlsverify"
+echo ""
 echo ""
 
-
-
+echo "[Info] Create folders for mosquitto"
 sudo mkdir /mosquitto
 sudo mkdir /mosquitto/config
-sudo docker run -it -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
+sudo mkdir /mosquitto/data
+sudo mkdir /mosquitto/log
+sudo chmod 757 mosquitto
+sudo chmod 757 mosquitto/config
+#sudo chmod 755 mosquitto/data
+#sudo chmod 755 mosquitto/log
 
+echo "[Info] Download mosquitto.conf for "
+sudo curl -sL https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/mosquitto.conf > "/mosquitto/config/mosquitto.conf"
+                                                                                                # "/var/lib/docker/volumes/mosquitto.conf"
 
-# Add TLS protected MQTT
-listener 8883
-cafile  /ssl/chain.pem
-certfile  /ssl/fullchain.pem
-keyfile  /ssl/privkey.pem
-
-listener 9001
-protocol websockets
-
-# Add TLS protected MQTT via websockets
-listener 8443
-protocol websockets
-cafile  /ssl/chain.pem
-certfile  /ssl/fullchain.pem
-keyfile  /ssl/privkey.pem
-
-persist data to /mosquitto/data
-log to /mosquitto/log/mosquitto.log
-
-
-
-–name=“mosquitto” –restart on-failure -p 1883:1883-p 1884:1884 -p 1883:8884 -p 1883:8883 -p 9001:9001 -v /srv/mqtt/config:/mqtt/config:ro  -v /srv/mqtt/log:/mqtt/log  -v /srv/mqtt/data/:/mqtt/data/ eclipse-mosquitto
-
-
-–name=“mosquitto” 
-–restart on-failure 
--p 1883:1883 
--p 9001:9001 
--v /srv/mqtt/config:/mqtt/config:ro 
--v /srv/mqtt/log:/mqtt/log 
--v /srv/mqtt/data/:/mqtt/data/ 
-eclipse-mosquitto
+sudo docker run -it –name=“mosquitto” -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
+# Example: sudo docker run -it –name=“mosquitto” –restart on-failure -p 1883:1883 -p 9001:9001 -p 1883:8884 -p 1883:8883 -p 9001:9001 -v /srv/mqtt/config:/mqtt/config:ro  -v /srv/mqtt/log:/mqtt/log  -v /srv/mqtt/data/:/mqtt/data/ eclipse-mosquitto
 
