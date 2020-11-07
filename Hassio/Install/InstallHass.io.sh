01f390635b1613897ca38a202cfdafc6806b418e
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-07 00:25:20 -0300

    Update InstallHass.io.sh

--- Hassio/Install/InstallHass.io.sh
+++ Hassio/Install/InstallHass.io.sh
@@ -35,8 +35,8 @@ sudo su
 echo ""
 echo ""
 echo ""
-echo "[Info] Install Home Assistant -"
-sudo curl -sL https://raw.githubusercontent.com/icanfixitweb/Hassio-Virtual-Machine/master/hassio_install_script/original_script/installer.sh | bash -s -- -d $DIR_HASSIO
+#echo "[Info] Install Home Assistant - from https://github.com/icanfixitweb/Hassio-Virtual-Machine/tree/master/hassio_install_script"
+#sudo curl -sL https://raw.githubusercontent.com/icanfixitweb/Hassio-Virtual-Machine/master/hassio_install_script/original_script/installer.sh | bash -s -- -d $DIR_HASSIO
 
 echo ""
 echo ""
@@ -83,8 +83,6 @@ echo ""
 echo ""
 
 
-
-
 #echo "[Info] Run mosquitto on docker "
            sudo docker run -it --name=mosquitto                       -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v /mosquitto/:/mosquitto/           -v /mosquitto/log:/mosquitto/log -v /mosquitto/data/:/mosquitto/data/  eclipse-mosquitto
 # Example: sudo docker run -it --name=“mosquitto” –restart on-failure -p 1883:1883 -p 1884:1884 -p 8883:8883 -p 8884:8884 -p 9001:9001 -v /srv/mqtt/config:/mqtt/config:ro  -v /srv/mqtt/log:/mqtt/log       -v /srv/mqtt/data/:/mqtt/data/        eclipse-mosquitto
@@ -107,23 +105,18 @@ sudo curl -sL https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main
 echo ""
 echo ""
 echo "Baixar o Portainerio" 
- sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /path/on/host/data:/data portainer/portainer
-#sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /path/to/certs:/certs -v portainer_data:/data portainer/portainer -H tcp://<DOCKER_HOST>:<DOCKER_PORT> --tlsverify
+sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /path/on/host/data:/data portainer/portainer
 echo ""
 echo ""
-
-
-#echo ""
-#echo ""
-#echo "[Info] Referencia de erros:"
-#echo "Para imagem docker https://hub.docker.com/_/eclipse-mosquitto"
-#echo "Para problemas de pasta com o parametro -v... https://github.com/eclipse/mosquitto/issues/457"
-#echo ""
-#echo ""
-#echo "[Info] Para Configuracoes no hassio"
-#echo "https://www.home-assistant.io/docs/mqtt/broker#run-your-own"
-#echo "https://community.home-assistant.io/t/solved-connect-ha-to-mqtt-broker-with-tls-ca-crt/158415"
-#echo ""
-#echo ""
-#echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/"
-#echo "(replace yourIPaddress with your actual IP address) from your web browser to access your new Home Assistant installation."
+echo "[Info] Referencia de erros:"
+echo "Para imagem docker https://hub.docker.com/_/eclipse-mosquitto"
+echo "Para problemas de pasta com o parametro -v... https://github.com/eclipse/mosquitto/issues/457"
+echo ""
+echo ""
+echo "[Info] Para Configuracoes no hassio"
+echo "https://www.home-assistant.io/docs/mqtt/broker#run-your-own"
+echo "https://community.home-assistant.io/t/solved-connect-ha-to-mqtt-broker-with-tls-ca-crt/158415"
+echo ""
+echo ""
+echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/"
+echo "(replace yourIPaddress with your actual IP address) from your web browser to access your new Home Assistant installation."
