f9ef81d4190b6546b02684dada2b29e7f96f303d
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-08 02:26:37 -0300

    Update certificates.sh

--- certificates/certificates.sh
+++ certificates/certificates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-info "Digite o nome do nome dominio no duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar O NOME_DO_DNS: "	info "Digite o nome do nome dominio duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar O NOME_DO_DNS: "
+info "Digite o nome do nome dominio no duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar o NOME_DO_DNS: "	info "Digite o nome do nome dominio duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar O NOME_DO_DNS: "
 read answer < /dev/tty
 
 sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/"$answer".duckdns.org/chain.pem /usr/share/hassio/ssl
