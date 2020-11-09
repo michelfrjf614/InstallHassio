4415dc6f39ea1163f4f8fbbca5c9c04d87bdcdc9
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-08 21:51:08 -0300

    Update certificates.sh

--- certificates/certificates.sh
+++ certificates/certificates.sh
@@ -8,7 +8,7 @@ sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/"$answer".duckdns
 cd /usr/share/hassio/ssl
 
 
-echo "Náo precisamos dos certificados para as portas a seguir:"
+echo "Nós precisamos dos certificados para as portas a seguir:"
 echo "listener 8884 - protocol websockets use:"
 echo "cafile /ssl/chain.pem"
 echo "certfile /ssl/cert.pem"
@@ -23,35 +23,30 @@ echo ""
 
 echo "#####################################################################################"
 
-echo "Extrair .CRT de .PEM:   fullchain.PEM ==> cert.CRT"
+echo "Extrair .CRT de .PEM:   fullchain.PEM ==> cert.crt"
 sudo openssl crl2pkcs7 -nocrl -certfile fullchain.pem | openssl pkcs7 -print_certs -out cert.crt
 echo ""
 
 
+
 echo "Firmwware use:"
 echo "Extrair .DER de .PEM:  chain.pem     ==> ca.der"
-sudo openssl x509 -outform der -in chain.pem -out ca.der
+#sudo openssl x509 -outform der -in chain.pem -out ca.der
+ sudo openssl x509 -outform DER -in chain.pem -out ca.der 
 echo ""
 
 echo "Extrair .DER de .PEM:  fullchain.pem ==> cert.der"
-sudo openssl x509 -outform der -in fullchain.pem -out cert.der
+#sudo openssl x509 -outform der -in fullchain.pem -out cert.der
+ sudo openssl x509 -outform DER -in fullchain.pem -out cert.der
 echo ""
 
 echo "Extrair .DER de .PEM:  privkey.pem   ==> private.der"
-sudo openssl x509 -outform der -in privkey.pem -out private.der
+#sudo openssl x509 -outform der -in privkey.pem -out private.der
+ sudo openssl rsa  -outform DER -in private.pem -out private.der
 echo ""
-
 echo ""
+echo "Certificado gerados"
 
 #certificate.crt
-#cert.csr
 #cert.key
 #cert.pem
-
-#echo "[Info] Para certificados"
-#echo "http://www.steves-internet-guide.com/mosquitto-tls/"
-#echo "https://www.youtube.com/watch?v=f3f4h7q6x5g"
-#echo "https://www.youtube.com/watch?v=zPT8LFWqazM"
-#echo ""
-#echo "https://www.sslshopper.com/ssl-converter.html"
-#echo ""
