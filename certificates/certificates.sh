3bf2b7a048dfa42e444b170669b9a58b51e47fba
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-11 08:06:32 -0300

    Update certificates.sh

--- certificates/certificates.sh
+++ certificates/certificates.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-info "Digite o nome do nome dominio no duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar o NOME_DO_DNS:"
-read answer < /dev/tty
-
-echo "Copiando o arquivo chain.pem referente ao endereço: "$answer".duckdns.org"
-sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/"$answer".duckdns.org/chain.pem /usr/share/hassio/ssl
-# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/chain.pem /usr/share/hassio/ssl
-# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/fullchain.pem /usr/share/hassio/ssl
-# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/privkey.pem /usr/share/hassio/ssl
-
-cd /usr/share/hassio/ssl
-
 echo "Nós precisamos dos certificados para as portas a seguir:"
+echo ""
 echo "listener 8884 - protocol websockets use:"
 echo "cafile /ssl/chain.pem"
 echo "certfile /ssl/cert.pem"
@@ -26,21 +16,36 @@ echo ""
 
 echo "#####################################################################################"
 
+cd /usr/share/hassio/ssl
+
+info "Digite o nome do nome dominio no duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar o NOME_DO_DNS:"
+read answer < /dev/tty
+
+echo "Copiando o arquivo chain.pem referente ao endereço: "$answer".duckdns.org"
+sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/"$answer".duckdns.org/chain.pem /usr/share/hassio/ssl
+echo ""
+
 echo "Extrair .CRT de .PEM:   fullchain.PEM ==> cert.crt"
 sudo  openssl crl2pkcs7 -nocrl -certfile fullchain.pem | openssl pkcs7 -print_certs -out cert.crt
 echo ""
 
-echo "Firmwware use:"
-echo "Extrair .DER de .PEM:  chain.pem     ==> ca.der"
+echo "to use Firmwware:"
+echo "    Extrair .DER de .PEM:  chain.pem     ==> ca.der"
 sudo openssl x509 -outform DER -in chain.pem -out ca.der
 echo ""
 
-echo "Extrair .DER de .PEM:  fullchain.pem ==> cert.der"
+echo "    Extrair .DER de .PEM:  fullchain.pem ==> cert.der"
 sudo openssl x509 -outform DER -in fullchain.pem -out cert.der
 echo ""
 
-echo "Extrair .DER de .PEM:  privkey.pem   ==> private.der"
+echo "    Extrair .DER de .PEM:  privkey.pem   ==> private.der"
 sudo openssl rsa  -outform DER -in privkey.pem -out private.der
 echo ""
-echo ""
 echo "Certificado gerados, acesse com endereço https://"$answer".duckdns.org.org:8123/" 
+echo ""
+echo ""
+
+
+# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/chain.pem /usr/share/hassio/ssl
+# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/fullchain.pem /usr/share/hassio/ssl
+# sudo cp /usr/share/hassio/addons/data/core_duckdns/letsencrypt/testexpto.duckdns.org/privkey.pem /usr/share/hassio/ssl
