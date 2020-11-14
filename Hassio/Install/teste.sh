b6017e32969f1dddc4a156a968ce084d09ebe1b1
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-14 11:53:05 -0300

    Update and rename teste to teste.sh

--- Hassio/Install/teste.sh
+++ Hassio/Install/teste.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
+
 info "Digite o nome do nome dominio no duckdns. Exemplo se for NOME_DO_DNS.duckdns.org informar o NOME_DO_DNS:"
 read answer < /dev/tty
 
 sed -i "s/smartboardneww/$answer/" /usr/share/hassio/homeassistant/configuration.yaml
 
 echo "Once the installation is completed go to http://hassio.local:8123/ or http://yourIPaddress:8123/ in the future http://$answer.duckdns.org/"
+
+
+
+#sudo curl -L https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/Hassio/Install/teste.sh | bash -s
