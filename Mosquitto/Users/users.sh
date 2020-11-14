5dd9901db45e83ccef6a118bf3aa9da947227b53
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-14 16:11:34 -0300

    Update users.sh

--- Mosquitto/Users/users.sh
+++ Mosquitto/Users/users.sh
@@ -6,6 +6,6 @@ echo "user [$1]" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist
 echo "topic readwrite #" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist
 echo "" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist
 
-
+# API deve chahar esse dois comandos
 #sudo curl -L https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/Mosquitto/Users/users.sh > users.sh
 #users.sh nome_do_usuario 
