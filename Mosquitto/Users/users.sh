3b115ca1f51763f1474a98df9ca6ead3d2c0e78a
MICHEL FRANCE VIEIRA TORRES <52437043+michelfrjf614@users.noreply.github.com>
2020-11-14 16:10:20 -0300

    Update users.sh

--- Mosquitto/Users/users.sh
+++ Mosquitto/Users/users.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Add user"
+echo "Add user [$1]"
 
 echo "user [$1]" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist_teste
 echo "topic readwrite #" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist_teste
 echo "" >> /usr/share/hassio/share/mosquitto/config/accesscontrollist_teste
 
 
-#sudo curl -L https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/Mosquitto/Users/users.sh | bash -s
-
+#sudo curl -L https://raw.githubusercontent.com/michelfrjf614/InstallHassio/main/Mosquitto/Users/users.sh > users.sh
+#users.sh nome_do_usuario 
